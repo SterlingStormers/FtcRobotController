949d5442c1d45dc0cf948396af0b4d8173110eba
Pixelotol7 <mattpsn2011@gmail.com>
2025-02-01 12:46:21 -0500

    redid reset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2024.java
@@ -90,9 +90,11 @@ if (robot.clawServo.getPosition() >= 1.05) {
        //Set the worm gear tol -7 degrees
 
         if (operator.right_bumper && operator.left_bumper && driver.right_bumper && driver.left_bumper) {
-            robot.wormGear.setPower(-1);
-            if (robot.wormGearAngle() == -7) {
+            if (robot.wormGearAngle() <= -7) {
                 robot.wormGear.setPower(0);
+            }
+            else {
+                robot.wormGear.setPower(-1);
             }
         }
         telemetry.addData("Lift","%.1f", opLeftStickY);
